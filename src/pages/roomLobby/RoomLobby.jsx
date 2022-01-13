9f4fad987aaf8b78b0c1dd37b510e8ecee2b33bf
Tanson Lee <leetanson@gmail.com>
2022-01-13 02:29:23 -0500

    location validation

--- src/pages/roomLobby/RoomLobby.jsx
+++ src/pages/roomLobby/RoomLobby.jsx
@@ -17,253 +17,258 @@ import Layout from "../../components/Layout";
 import UserList from "../../components/UserList";
 import ClipboardCopy from "../../components/ClipboardCopy";
 
-const RoomLobby = ({
-  isOwner,
-  setAllRestaurants,
-  roomId,
-  setAppUsers,
-  setIsOwner,
-}) => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const RoomLobby = ({ isOwner, setAllRestaurants, roomId, setAppUsers, setIsOwner }) => {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [users, setUsers] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [locationError, setLocationError] = useState(false);
+	const [locationEdited, setLocationEdited] = useState(false);
 
-  const [loc, setLoc] = useState("");
-  const [maxDistance, setMaxDistance] = useState(5);
-  const [prices, setPrices] = useState([1, 2]); // "$" or "$, $$" or "$$$$" etc.. (can be 1, 2, 3, and 4)
+	const [loc, setLoc] = useState("");
+	const [maxDistance, setMaxDistance] = useState(5);
+	const [prices, setPrices] = useState([1, 2]); // "$" or "$, $$" or "$$$$" etc.. (can be 1, 2, 3, and 4)
 
-  const handleChange = (setValue) => (event) => {
-    setValue(event.target.value);
-  };
+	const handleChange = setValue => event => {
+		setValue(event.target.value);
+	};
 
-  useEffect(() => {
-    if (!location.state || !location.state.roomId) {
-      navigate("/");
-      return;
-    }
-    console.log(
-      `emit JOIN_ROOM: username=${location.state.username}, roomId=${location.state.roomId}`
-    );
-    socket.emit("JOIN_ROOM", {
-      username: location.state.username,
-      roomId: location.state.roomId,
-      isOwner: isOwner,
-    });
-    socket.on("NEW_ROOM_USERS", ({ users: allUsers }) => {
-      console.log(`on NEW_ROOM_USERS: newUsers=${allUsers}`);
-      setUsers(allUsers);
-      setAppUsers(allUsers);
-      // check if a new owner is assigned to be me
-      const myUserData = allUsers.find(
-        (user) => user.userId === location.state.userId
-      );
-      if (myUserData && myUserData.isOwner) {
-        setIsOwner(true);
-      }
-      console.log("users: ", allUsers);
-    });
-    socket.on("FOUND_RESTAURANTS", (totalRestaurantList) => {
-      console.log(
-        `on FOUND_RESTAURANTS: totalRestaurantList=very long list of restaurants`
-      );
-      console.log(`going to the Search page`);
-      setAllRestaurants(totalRestaurantList);
-      setIsLoading(false);
-      navigate("/search", {
-        users: users,
-      });
-    });
-  }, [
-    setAllRestaurants,
-    isOwner,
-    location.state?.roomId,
-    location.state?.username,
-    navigate,
-  ]);
+	useEffect(() => {
+		if (!location.state || !location.state.roomId) {
+			navigate("/");
+			return;
+		}
+		console.log(
+			`emit JOIN_ROOM: username=${location.state.username}, roomId=${location.state.roomId}`
+		);
+		socket.emit("JOIN_ROOM", {
+			username: location.state.username,
+			roomId: location.state.roomId,
+			isOwner: isOwner,
+		});
+		socket.on("NEW_ROOM_USERS", ({ users: allUsers }) => {
+			console.log(`on NEW_ROOM_USERS: newUsers=${allUsers}`);
+			setUsers(allUsers);
+			setAppUsers(allUsers);
+			// check if a new owner is assigned to be me
+			const myUserData = allUsers.find(user => user.userId === location.state.userId);
+			if (myUserData && myUserData.isOwner) {
+				setIsOwner(true);
+			}
+			console.log("users: ", allUsers);
+		});
+		socket.on("FOUND_RESTAURANTS", data => {
+			console.log("data", data);
+			console.log(`on FOUND_RESTAURANTS: totalRestaurantList=very long list of restaurants`);
+			if (data.success) {
+				setAllRestaurants(data.restaurants);
+				setIsLoading(false);
+				navigate("/search", {
+					users: users,
+				});
+			} else {
+				console.log("FOUND_RESTAURANTS: failed to find restaurants");
+				setLocationError(true);
+			}
+		});
+	}, [setAllRestaurants, isOwner, location.state?.roomId, location.state?.username, navigate]);
 
-  useEffect(() => {
-    if (!roomId || roomId.length !== 6) {
-      navigate("/");
-      return;
-    }
-    if (!navigator.geolocation) return;
+	useEffect(() => {
+		if (!roomId || roomId.length !== 6) {
+			navigate("/");
+			return;
+		}
+		if (!navigator.geolocation) return;
 
-    return navigator.geolocation.getCurrentPosition(async (position) => {
-      const response = await axios.get(
-        `${api}/address/${position.coords.latitude},${position.coords.longitude}`
-      );
+		return navigator.geolocation.getCurrentPosition(async position => {
+			const response = await axios.get(
+				`${api}/address/${position.coords.latitude},${position.coords.longitude}`
+			);
 
-      if (response.data.success) {
-        setLoc(response.data.address);
-      }
-    });
-  }, [navigate, roomId]);
+			console.log("locationEdited: ", locationEdited);
+			if (response.data.success && !locationEdited) {
+				setLoc(response.data.address);
+			}
+		});
+	}, [navigate, roomId, locationEdited]);
 
-  // convert from km to m
-  const formatDistance = (distance) => distance * 1000;
+	// convert from km to m
+	const formatDistance = distance => distance * 1000;
 
-  const formatPrices = (prices) => {
-    if (prices[0] == prices[1]) {
-      return prices[0];
-    } else {
-      let priceArray = [];
-      for (let i = prices[0]; i <= prices[1]; ++i) {
-        priceArray.push(i);
-      }
-      return priceArray.join(", ");
-    }
-  };
+	const formatPrices = prices => {
+		if (prices[0] == prices[1]) {
+			return prices[0];
+		} else {
+			let priceArray = [];
+			for (let i = prices[0]; i <= prices[1]; ++i) {
+				priceArray.push(i);
+			}
+			return priceArray.join(", ");
+		}
+	};
 
-  const handleNext = () => {
-    const location = "Waterloo, Ontario";
-    console.log(`owner clicked next`);
-    console.log(`emit GET_RESTAURANTS: location=${location}`);
-    socket.emit("GET_RESTAURANTS", {
-      location: loc,
-      radius: formatDistance(maxDistance),
-      price: formatPrices(prices),
-    });
-    setIsLoading(true);
-  };
+	const handleNext = () => {
+		const location = "Waterloo, Ontario";
+		console.log(`owner clicked next`);
+		console.log(`emit GET_RESTAURANTS: location=${location}`);
+		socket.emit("GET_RESTAURANTS", {
+			location: loc,
+			radius: formatDistance(maxDistance),
+			price: formatPrices(prices),
+		});
+		setIsLoading(true);
+	};
 
-  const distanceText = (value) => `${value} km`;
-  const distanceMarks = [
-    {
-      value: 1,
-      label: "1 km",
-    },
-    {
-      value: 25,
-      label: "25 km",
-    },
-  ];
+	const distanceText = value => `${value} km`;
+	const distanceMarks = [
+		{
+			value: 1,
+			label: "1 km",
+		},
+		{
+			value: 25,
+			label: "25 km",
+		},
+	];
 
-  const priceText = (value) => "$".repeat(value);
-  const priceMarks = [
-    {
-      value: 1,
-      label: "$",
-    },
-    {
-      value: 2,
-      label: "$$",
-    },
-    {
-      value: 3,
-      label: "$$$",
-    },
-    {
-      value: 4,
-      label: "$$$$",
-    },
-  ];
+	const priceText = value => "$".repeat(value);
+	const priceMarks = [
+		{
+			value: 1,
+			label: "$",
+		},
+		{
+			value: 2,
+			label: "$$",
+		},
+		{
+			value: 3,
+			label: "$$$",
+		},
+		{
+			value: 4,
+			label: "$$$$",
+		},
+	];
 
-  return (
-    <Layout extendPaper={true}>
-      <Stack direction="row" justifyContent="center" height="100%">
-        <Box
-          sx={{
-            borderRadius: "2%",
-            minWidth: "33%",
-          }}>
-          <Typography
-            variant="h1"
-            component="h1"
-            sx={{ fontSize: "3rem", marginTop: "1rem", textAlign: "center" }}>
-            Room Lobby
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            gutterBottom
-            component="p"
-            sx={{ mt: 2, textAlign: "center" }}>
-            Send your friends the following Room ID
-          </Typography>
-          <ClipboardCopy copyText={location.state?.roomId} />
-          <Box>
-            <UserList users={users} myUserId={socket.id} />
-          </Box>
-          {isOwner ? (
-            <Box>
-              <Box>
-                <Typography variant="h6" component="h6" sx={{ mt: 2 }}>
-                  Search Settings
-                </Typography>
-                <Box sx={{ p: 1 }}>
-                  <TextField
-                    sx={{ width: "100%" }}
-                    id="outlined-name"
-                    required
-                    label="Location"
-                    value={loc}
-                    onChange={(e) => setLoc(e.target.value)}
-                    color="secondary"
-                  />
-                  <FormHelperText sx={{ pl: 1 }} id="component-helper-text">
-                    "Waterloo, ON", "Toronto", "M5B 2H1"
-                  </FormHelperText>
-                </Box>
-                <Box sx={{ p: 1 }}>
-                  <Typography variant="subtitle1" component="h5">
-                    Max Distance
-                  </Typography>
+	return (
+		<Layout extendPaper={true}>
+			<Stack direction="row" justifyContent="center" height="100%">
+				<Box
+					sx={{
+						borderRadius: "2%",
+						minWidth: "33%",
+					}}
+				>
+					<Typography
+						variant="h1"
+						component="h1"
+						sx={{ fontSize: "3rem", marginTop: "1rem", textAlign: "center" }}
+					>
+						Room Lobby
+					</Typography>
+					<Typography
+						variant="subtitle1"
+						gutterBottom
+						component="p"
+						sx={{ mt: 2, textAlign: "center" }}
+					>
+						Send your friends the following Room ID
+					</Typography>
+					<ClipboardCopy copyText={location.state?.roomId} />
+					<Box>
+						<UserList users={users} myUserId={socket.id} />
+					</Box>
+					{isOwner ? (
+						<Box>
+							<Box>
+								<Typography variant="h6" component="h6" sx={{ mt: 2 }}>
+									Search Settings
+								</Typography>
+								<Box sx={{ p: 1 }}>
+									<TextField
+										sx={{ width: "100%" }}
+										id="outlined-name"
+										required
+										label="Location"
+										value={loc}
+										onChange={e => {
+											setLoc(e.target.value);
+											setLocationError(false);
+											setLocationEdited(true);
+										}}
+										color="secondary"
+										error={locationError}
+									/>
+									<FormHelperText sx={{ pl: 1 }} id="component-helper-text">
+										{locationError
+											? "Invalid location"
+											: `"Waterloo, ON",
+											"Toronto",
+											"M5B 2H1"`}
+									</FormHelperText>
+								</Box>
+								<Box sx={{ p: 1 }}>
+									<Typography variant="subtitle1" component="h5">
+										Max Distance
+									</Typography>
 
-                  <Slider
-                    aria-label="Max Distance (km)"
-                    defaultValue={5}
-                    onChange={handleChange(setMaxDistance)}
-                    getAriaValueText={distanceText}
-                    valueLabelFormat={distanceText}
-                    valueLabelDisplay="auto"
-                    step={1}
-                    marks={distanceMarks}
-                    min={1}
-                    max={25}
-                    color="secondary"
-                  />
-                </Box>
-                <Box sx={{ p: 1 }}>
-                  <Typography variant="subtitle1" component="h5">
-                    Price Range
-                  </Typography>
-                  <Slider
-                    getAriaLabel={() => "Price range"}
-                    value={prices}
-                    onChange={handleChange(setPrices)}
-                    getAriaValueText={priceText}
-                    valueLabelFormat={priceText}
-                    valueLabelDisplay="auto"
-                    marks={priceMarks}
-                    min={1}
-                    max={4}
-                    sx={{ color: "secondary" }}
-                    color="secondary"
-                  />
-                </Box>
-              </Box>
-              <Box textAlign="center" sx={{ m: 4 }}>
-                <Button
-                  variant="contained"
-                  // color="secondary"
-                  size="large"
-                  // loading={isLoading}
-                  onClick={handleNext}>
-                  Start
-                </Button>
-              </Box>
-            </Box>
-          ) : (
-            <Box textAlign="center" sx={{ m: 4 }}>
-              <Button variant="contained" disabled>
-                Waiting for owner...
-              </Button>
-            </Box>
-          )}
-        </Box>
-      </Stack>
-    </Layout>
-  );
+									<Slider
+										aria-label="Max Distance (km)"
+										defaultValue={5}
+										onChange={handleChange(setMaxDistance)}
+										getAriaValueText={distanceText}
+										valueLabelFormat={distanceText}
+										valueLabelDisplay="auto"
+										step={1}
+										marks={distanceMarks}
+										min={1}
+										max={25}
+										color="secondary"
+									/>
+								</Box>
+								<Box sx={{ p: 1 }}>
+									<Typography variant="subtitle1" component="h5">
+										Price Range
+									</Typography>
+									<Slider
+										getAriaLabel={() => "Price range"}
+										value={prices}
+										onChange={handleChange(setPrices)}
+										getAriaValueText={priceText}
+										valueLabelFormat={priceText}
+										valueLabelDisplay="auto"
+										marks={priceMarks}
+										min={1}
+										max={4}
+										sx={{ color: "secondary" }}
+										color="secondary"
+									/>
+								</Box>
+							</Box>
+							<Box textAlign="center" sx={{ m: 4 }}>
+								<Button
+									variant="contained"
+									// color="secondary"
+									size="large"
+									// loading={isLoading}
+									onClick={handleNext}
+								>
+									Start
+								</Button>
+							</Box>
+						</Box>
+					) : (
+						<Box textAlign="center" sx={{ m: 4 }}>
+							<Button variant="contained" disabled>
+								Waiting for owner...
+							</Button>
+						</Box>
+					)}
+				</Box>
+			</Stack>
+		</Layout>
+	);
 };
 
 export default RoomLobby;
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -18,79 +18,72 @@ const MATCHES_TAB = 0;
 const RESTAURANTS_TAB = 1;
 const USERS_TAB = 2;
 
-const Search = ({
-  setMatchedRestaurants,
-  matchedRestaurants,
-  allRestaurants,
-  roomId,
-  users,
-}) => {
-  const navigate = useNavigate();
+const Search = ({ setMatchedRestaurants, matchedRestaurants, allRestaurants, roomId, users }) => {
+	const navigate = useNavigate();
 
-  const [tab, setTab] = useState(RESTAURANTS_TAB);
-  const [restaurantIndex, setRestaurantIndex] = useState(0);
+	const [tab, setTab] = useState(RESTAURANTS_TAB);
+	const [restaurantIndex, setRestaurantIndex] = useState(0);
 
-  const acceptRestaurant = () => {
-    socket.emit("ACCEPT_RESTAURANT", {
-      restaurantId: allRestaurants[restaurantIndex].id,
-    });
-  };
+	const acceptRestaurant = () => {
+		socket.emit("ACCEPT_RESTAURANT", {
+			restaurantId: allRestaurants[restaurantIndex].id,
+		});
+	};
 
-  const canDisplay = (displayTab) => {
-    if (displayTab === tab) {
-      return "block";
-    } else {
-      return "none";
-    }
-  };
+	const canDisplay = displayTab => {
+		if (displayTab === tab) {
+			return "block";
+		} else {
+			return "none";
+		}
+	};
 
-  const swipeNext = (isAccept) => {
-    if (restaurantIndex === allRestaurants.length - 1) {
-      setRestaurantIndex(0);
-    } else {
-      setRestaurantIndex(restaurantIndex + 1);
-    }
-    if (isAccept) {
-      acceptRestaurant();
-    }
-  };
+	const swipeNext = isAccept => {
+		if (restaurantIndex === allRestaurants.length - 1) {
+			setRestaurantIndex(0);
+		} else {
+			setRestaurantIndex(restaurantIndex + 1);
+		}
+		if (isAccept) {
+			acceptRestaurant();
+		}
+	};
 
-  useEffect(() => {
-    if (!roomId || roomId.length !== 6) {
-      navigate("/");
-    }
-    socket.on("MATCHES_FOUND", (matches) => {
-      console.log(`on MATCHES_FOUND: matches=${matches}`);
-      const matchingRestaurants = allRestaurants.filter((restaurant) =>
-        matches.includes(restaurant.id)
-      );
-      console.log(
-        `matchingRestaurants: ${matchingRestaurants.map((e) => e.name)}`
-      );
-      setMatchedRestaurants(matchingRestaurants);
-    });
-  }, [allRestaurants, setMatchedRestaurants, navigate, roomId]);
+	useEffect(() => {
+		if (!roomId || roomId.length !== 6) {
+			navigate("/");
+		}
+		socket.on("MATCHES_FOUND", matches => {
+			console.log(`on MATCHES_FOUND: matches=${matches}`);
+			const matchingRestaurants = allRestaurants.filter(restaurant =>
+				matches.includes(restaurant.id)
+			);
+			console.log(`matchingRestaurants: ${matchingRestaurants.map(e => e.name)}`);
+			setMatchedRestaurants(matchingRestaurants);
+		});
+	}, [allRestaurants, setMatchedRestaurants, navigate, roomId]);
 
-  return (
-    <Layout extendPaper={true}>
-      {allRestaurants.length === 0 ? (
-        <Typography variant="p" component="div">
-          Loading...
-        </Typography>
-      ) : (
-        <Grid container spacing={0} sx={{ p: 4 }}>
-          <Grid
-            item
-            sm={12}
-            md={4}
-            sx={{
-              display: {
-                xs: canDisplay(MATCHES_TAB),
-                md: "block",
-              },
-            }}>
-            <Matches matchedRestaurants={matchedRestaurants} />
-            {/* {matchedRestaurants.map((rest, index) => {
+	return (
+		<Layout extendPaper={true}>
+			{allRestaurants.length === 0 ? (
+				<Typography variant="p" component="div">
+					Loading...
+				</Typography>
+			) : (
+				<Grid container spacing={0} sx={{ p: 4, textAlign: "center" }}>
+					<Grid
+						item
+						sm={12}
+						md={4}
+						sx={{
+							display: {
+								xs: canDisplay(MATCHES_TAB),
+								md: "block",
+							},
+						}}
+					>
+						<Matches matchedRestaurants={matchedRestaurants} />
+						{/* {matchedRestaurants.map((rest, index) => {
               return (
                 <div>
                   <Typography variant="p" component="p" key={index}>
@@ -99,79 +92,82 @@ const Search = ({
                 </div>
               );
             })} */}
-          </Grid>
-          <Grid
-            item
-            sm={12}
-            md={4}
-            sx={{
-              display: {
-                xs: canDisplay(RESTAURANTS_TAB),
-                md: "block",
-              },
-            }}>
-            <Restaurant
-              {...allRestaurants[restaurantIndex]}
-              swipeNext={swipeNext}
-            />
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-around",
-              }}>
-              <IconButton
-                sx={{
-                  backgroundColor: "error.light",
-                  "&:hover": {
-                    backgroundColor: "error.main",
-                  },
-                  width: "5rem",
-                  height: "5rem",
-                }}
-                variant="contained"
-                onClick={() => {
-                  swipeNext(false);
-                }}>
-                <CloseIcon sx={{ fontSize: "2.5rem" }} />
-              </IconButton>
-              <IconButton
-                size="large"
-                sx={{
-                  backgroundColor: "success.main",
-                  "&:hover": {
-                    backgroundColor: "success.dark",
-                  },
-                  width: "5rem",
-                  height: "5rem",
-                }}
-                variant="contained"
-                onClick={() => {
-                  swipeNext(true);
-                }}>
-                <CheckIcon sx={{ fontSize: "2.5rem" }} />
-              </IconButton>
-            </Box>
-          </Grid>
-          <Grid
-            item
-            sm={12}
-            md={4}
-            sx={{
-              pl: 8,
-              pr: 8,
-              display: {
-                xs: canDisplay(USERS_TAB),
-                md: "block",
-              },
-            }}
-            align="center">
-            <UserList users={users} />
-          </Grid>
-        </Grid>
-      )}
-      <BottomNavbar tab={tab} setTab={setTab} />
-    </Layout>
-  );
+					</Grid>
+					<Grid
+						item
+						sm={12}
+						md={4}
+						sx={{
+							display: {
+								xs: canDisplay(RESTAURANTS_TAB),
+								md: "block",
+							},
+							margin: "0 auto",
+						}}
+					>
+						<Restaurant {...allRestaurants[restaurantIndex]} swipeNext={swipeNext} />
+						<Box
+							sx={{
+								display: "flex",
+								justifyContent: "space-around",
+							}}
+						>
+							<IconButton
+								sx={{
+									backgroundColor: "error.light",
+									"&:hover": {
+										backgroundColor: "error.main",
+									},
+									width: "5rem",
+									height: "5rem",
+								}}
+								variant="contained"
+								onClick={() => {
+									swipeNext(false);
+								}}
+							>
+								<CloseIcon sx={{ fontSize: "2.5rem" }} />
+							</IconButton>
+							<IconButton
+								size="large"
+								sx={{
+									backgroundColor: "success.main",
+									"&:hover": {
+										backgroundColor: "success.dark",
+									},
+									width: "5rem",
+									height: "5rem",
+								}}
+								variant="contained"
+								onClick={() => {
+									swipeNext(true);
+								}}
+							>
+								<CheckIcon sx={{ fontSize: "2.5rem" }} />
+							</IconButton>
+						</Box>
+					</Grid>
+					<Grid
+						item
+						sm={12}
+						md={4}
+						sx={{
+							pl: 8,
+							pr: 8,
+							display: {
+								xs: canDisplay(USERS_TAB),
+								md: "block",
+							},
+						}}
+						align="center"
+					>
+						<UserList users={users} />
+					</Grid>
+				</Grid>
+			)}
+			<BottomNavbar tab={tab} setTab={setTab} />
+		</Layout>
+	);
 };
 
 export default Search;
