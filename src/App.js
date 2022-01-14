5d181b5622714f2a8f453d0a27756e957bd030fb
Tanson Lee <leetanson@gmail.com>
2022-01-13 20:37:06 -0500

    solve warnings for production

--- src/App.js
+++ src/App.js
@@ -9,70 +9,62 @@ import CreateRoom from "./pages/createRoom/CreateRoom";
 import EnterRoom from "./pages/enterRoom/EnterRoom";
 import RoomLobby from "./pages/roomLobby/RoomLobby";
 import Search from "./pages/search/Search";
-import About from "./pages/about/About";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const App = () => {
-  const [isOwner, setIsOwner] = useState(false);
-  const [matchedRestaurants, setMatchedRestaurants] = useState([]);
-  const [allRestaurants, setAllRestaurants] = useState([]);
-  const [roomId, setRoomId] = useState(null);
-  const [appUsers, setAppUsers] = useState([]);
+	const [isOwner, setIsOwner] = useState(false);
+	const [matchedRestaurants, setMatchedRestaurants] = useState([]);
+	const [allRestaurants, setAllRestaurants] = useState([]);
+	const [roomId, setRoomId] = useState(null);
+	const [appUsers, setAppUsers] = useState([]);
 
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-      background: {
-        paper: "#0D1C2D",
-      },
-    },
-  });
+	const darkTheme = createTheme({
+		palette: {
+			mode: "dark",
+			background: {
+				paper: "#0D1C2D",
+			},
+		},
+	});
 
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <Routes>
-          <Route path="/" element={<Home setIsOwner={setIsOwner} />} />
-          <Route
-            path="/create-room"
-            element={<CreateRoom setRoomId={setRoomId} />}
-          />
-          <Route
-            path="/enter-room"
-            element={<EnterRoom setRoomId={setRoomId} />}
-          />
-          <Route
-            path="/room-lobby"
-            element={
-              <RoomLobby
-                isOwner={isOwner}
-                setIsOwner={setIsOwner}
-                setAllRestaurants={setAllRestaurants}
-                roomId={roomId}
-                setAppUsers={setAppUsers}
-              />
-            }
-          />
-          <Route
-            path="/search"
-            element={
-              <Search
-                setMatchedRestaurants={setMatchedRestaurants}
-                matchedRestaurants={matchedRestaurants}
-                allRestaurants={allRestaurants}
-                roomId={roomId}
-                users={appUsers}
-              />
-            }
-          />
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={darkTheme}>
+				<Routes>
+					<Route path="/" element={<Home setIsOwner={setIsOwner} />} />
+					<Route path="/create-room" element={<CreateRoom setRoomId={setRoomId} />} />
+					<Route path="/enter-room" element={<EnterRoom setRoomId={setRoomId} />} />
+					<Route
+						path="/room-lobby"
+						element={
+							<RoomLobby
+								isOwner={isOwner}
+								setIsOwner={setIsOwner}
+								setAllRestaurants={setAllRestaurants}
+								roomId={roomId}
+								setAppUsers={setAppUsers}
+							/>
+						}
+					/>
+					<Route
+						path="/search"
+						element={
+							<Search
+								setMatchedRestaurants={setMatchedRestaurants}
+								matchedRestaurants={matchedRestaurants}
+								allRestaurants={allRestaurants}
+								roomId={roomId}
+								users={appUsers}
+							/>
+						}
+					/>
 
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
-  );
+					<Route path="*" element={<Navigate to="/" />} />
+				</Routes>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- src/pages/search/Restaurant.jsx
+++ src/pages/search/Restaurant.jsx
@@ -1,78 +1,72 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
 import Rating from "@mui/material/Rating";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 
-const Restaurant = (props) => {
-  // ADD STYLE FOR SWIPING RIGHT/LEFT
+const Restaurant = props => {
+	// ADD STYLE FOR SWIPING RIGHT/LEFT
 
-  const reformatDistance = (distance) => {
-    const kmValue = Math.round(distance / 100) / 10;
-    return kmValue + " km";
-  };
+	const reformatDistance = distance => {
+		const kmValue = Math.round(distance / 100) / 10;
+		return kmValue + " km";
+	};
 
-  return (
-    <Box textAlign="center">
-      <Typography variant="h4" component="h6">
-        {props.name}
-      </Typography>
-      <Typography variant="subtitle1" component="h1" color="text.secondary">
-        Categories:&nbsp;
-        {
-          props.categories.map((category) => category.title).join(", ")
-          // maybe display categories as diff colours?
-        }
-      </Typography>
-      <Box sx={{ p: 2 }}>
-        <img
-          style={{
-            width: "250px",
-            height: "250px",
-            objectFit: "cover",
-            maxWidth: "100%",
-          }}
-          src={props.image_url}
-          alt={props.name}
-        />
-      </Box>
-      <Typography variant="subtitle1" component="div" color="text.secondary">
-        {props.location.display_address.map(
-          (addressComponent) => " " + addressComponent
-        )}
-      </Typography>
-      <Typography variant="h6" component="div">
-        Distance: {reformatDistance(props.distance)}
-      </Typography>
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        sx={{ p: 1, pl: 6, pr: 6 }}>
-        <Grid item xs={4} align="end">
-          <Typography variant="subtitle1" component="div">
-            Price: {props.price}
-          </Typography>
-        </Grid>
-        <Grid item xs={8}>
-          <Rating value={props.rating} precision={0.2} readOnly />
-          <Typography align="center" variant="body2" component="div">
-            ({props.review_count.toLocaleString()} reviews)
-          </Typography>
-        </Grid>
-      </Grid>
-      <Link
-        variant="subtitle2"
-        href={props.url}
-        underline="hover"
-        target="_blank"
-        rel="noopener">
-        View on Yelp
-      </Link>
-    </Box>
-  );
+	return (
+		<Box textAlign="center">
+			<Typography variant="h4" component="h6">
+				{props.name}
+			</Typography>
+			<Typography variant="subtitle1" component="h1" color="text.secondary">
+				Categories:&nbsp;
+				{
+					props.categories.map(category => category.title).join(", ")
+					// maybe display categories as diff colours?
+				}
+			</Typography>
+			<Box sx={{ p: 2 }}>
+				<img
+					style={{
+						width: "250px",
+						height: "250px",
+						objectFit: "cover",
+						maxWidth: "100%",
+					}}
+					src={props.image_url}
+					alt={props.name}
+				/>
+			</Box>
+			<Typography variant="subtitle1" component="div" color="text.secondary">
+				{props.location.display_address.map(addressComponent => " " + addressComponent)}
+			</Typography>
+			<Typography variant="h6" component="div">
+				Distance: {reformatDistance(props.distance)}
+			</Typography>
+			<Grid container justifyContent="center" alignItems="center" sx={{ p: 1, pl: 6, pr: 6 }}>
+				<Grid item xs={4} align="end">
+					<Typography variant="subtitle1" component="div">
+						Price: {props.price}
+					</Typography>
+				</Grid>
+				<Grid item xs={8}>
+					<Rating value={props.rating} precision={0.2} readOnly />
+					<Typography align="center" variant="body2" component="div">
+						({props.review_count.toLocaleString()} reviews)
+					</Typography>
+				</Grid>
+			</Grid>
+			<Link
+				variant="subtitle2"
+				href={props.url}
+				underline="hover"
+				target="_blank"
+				rel="noopener"
+			>
+				View on Yelp
+			</Link>
+		</Box>
+	);
 };
 
 export default Restaurant;
