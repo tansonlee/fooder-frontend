57da6517ef8835470a9564df349b83554c69b970
Joseph Perez <perezj0230@gmail.com>
2023-05-08 16:56:51 -0400

    fooder on AWS

--- src/env.js
+++ src/env.js
@@ -1,5 +1,6 @@
 // export const api = "http://localhost:3001";
 // export const endpoint = "http://localhost:3000";
 // export const api = "https://app-fooder.herokuapp.com";
-export const api = 'https://fooder-backend-0d1i.onrender.com';
-export const endpoint = 'https://fooderweb.netlify.app';
+// export const api = 'https://fooder-backend-0d1i.onrender.com';
+export const api = "http://fooder.us-east-2.elasticbeanstalk.com/";
+export const endpoint = "https://fooderweb.netlify.app";
